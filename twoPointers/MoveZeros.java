37f674b1863e0e3e8f08dbcf2e5daceab86b7645
halder_410 <sneha2020sonu@gmail.com>
2023-10-30 23:49:38 +0530

    move zeros

--- twoPointers/MoveZeros.java
+++ twoPointers/MoveZeros.java
@@ -1,2 +1,22 @@
-package twoPointers;public class MoveZeros {
+package twoPointers;
+
+public class MoveZeros {
+    public static void main(String[] args) {
+        moveZeroes(new int[] {0,1,0,3,12});
+    }
+    public static void moveZeroes(int[] nums) {
+        int i=0;
+        int j=0;
+        while(j<nums.length-1) {
+            if(j==0) {
+                j++;
+            }
+            else {
+                nums[i] = nums[j];
+                nums[j] = 0;
+                i++;
+                j++;
+            }
+        }
+    }
 }
